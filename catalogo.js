2623701c4d891f7ba049b2bb7f42bb0d1e3f5fc2
July Suk <humbertoxd25@gmail.com>
2023-10-17 15:22:27 -0700

    CRUD

--- catalogo.js
+++
@@ -1,9 +0,0 @@
-// catalogo.js
-const catalogo = [
-    { id: 1, descripcion: "Producto 1", imagen: "producto1.jpg", precio: 40 },
-    { id: 2, descripcion: "Producto 2", imagen: "producto2.jpg", precio: 50 },
-    { id: 3, descripcion: "Producto 3", imagen: "producto3.jpg", precio: 55 },
-    { id: 4, descripcion: "Producto 4", imagen: "producto4.jpg", precio: 30 },
-    { id: 5, descripcion: "Producto 5", imagen: "producto5.jpg", precio: 45 },
-    { id: 6, descripcion: "Producto 6", imagen: "producto6.jpg", precio: 55 },
-];
--- scriptCatalogo.js
+++ scriptCatalogo.js
@@ -1,9 +1,7 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const agregarProductoForm = document.getElementById("agregarProductoForm");
     const tablaProductos = document.getElementById("tablaProductos");
     const catalogo = [];
-
     // Función para actualizar la tabla de productos
     function actualizarTablaProductos() {
         // Limpiar la tabla actual
@@ -17,7 +15,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 <td>${producto.descripcion}</td>
                 <td><img src="${producto.imagen}" alt="${producto.descripcion}" height="60" width="100"></td>
                 <td>$${producto.precio}</td>
-                <td><button class="btn btn-danger eliminar-producto" data-id="${producto.id}">Eliminar</button></td>
+                <td><button class="btn btn-danger eliminar-producto" data-id="${producto.id}">Eliminar</button>
+                <button class="btn btn-primary editar-producto" data-id="${producto.id}">Editar</button>
+                </td>
             `;
             tablaProductos.appendChild(fila);
         });
@@ -65,19 +65,25 @@ document.addEventListener("DOMContentLoaded", function () {
     // Manejar el envío del formulario
     agregarProductoForm.addEventListener("submit", function (event) {
         event.preventDefault();
-
+    
         // Obtener los valores ingresados en el formulario
         const nuevoId = document.getElementById("nuevoId").value;
         const nuevoNombre = document.getElementById("nuevoNombre").value;
         const nuevaImagen = document.getElementById("nuevaImagen").value;
         const nuevoPrecio = parseFloat(document.getElementById("nuevoPrecio").value);
-
+    
         // Validar que el ID sea numérico
         if (!/^\d+$/.test(nuevoId)) {
             alert("El ID debe ser un valor numérico.");
             return;
         }
-
+    
+        // Validar que el precio no sea negativo
+        if (nuevoPrecio < 0) {
+            alert("El precio no puede ser negativo.");
+            return;
+        }
+    
         // Verificar si el ID ya existe en el catálogo
         if (idExiste(nuevoId)) {
             const confirmar = confirm("El ID ya está en uso. ¿Deseas reemplazar el producto existente?");
@@ -87,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // Eliminar el producto existente antes de agregar el nuevo
             eliminarProducto(nuevoId);
         }
-
+    
         // Crear un nuevo objeto de producto
         const nuevoProducto = {
             id: nuevoId,
@@ -95,16 +101,16 @@ document.addEventListener("DOMContentLoaded", function () {
             imagen: nuevaImagen,
             precio: nuevoPrecio,
         };
-
+    
         // Agregar el nuevo producto al catálogo
         catalogo.push(nuevoProducto);
-
+    
         // Almacenar el catálogo actualizado en el almacenamiento local
         localStorage.setItem("catalogo", JSON.stringify(catalogo));
-
+    
         // Actualizar la tabla de productos
         actualizarTablaProductos();
-
+    
         // Limpiar el formulario
         agregarProductoForm.reset();
     });
@@ -115,7 +121,78 @@ document.addEventListener("DOMContentLoaded", function () {
         catalogo.push(...catalogoGuardado);
         actualizarTablaProductos();
     }
-    
+
+       // Agrega un evento para manejar el clic en los botones de edición
+       tablaProductos.addEventListener("click", function (event) {
+        if (event.target.classList.contains("editar-producto")) {
+            const productoId = event.target.getAttribute("data-id");
+            mostrarFormularioEdicion(productoId);
+        }
+    });
+
+    // Función para mostrar el formulario de edición con los detalles del producto seleccionado
+    function mostrarFormularioEdicion(id) {
+        const producto = catalogo.find((p) => p.id === id);
+        if (producto) {
+            // Rellenar el formulario de edición con los detalles del producto
+            document.getElementById("editarId").value = producto.id;
+            document.getElementById("editarNombre").value = producto.descripcion;
+            document.getElementById("editarImagen").value = producto.imagen;
+            document.getElementById("editarPrecio").value = producto.precio;
+
+            // Mostrar el fondo difuminado y el formulario de edición
+            document.querySelector(".overlay").style.display = "block";
+            document.querySelector(".modal").style.display = "block";
+        }
+    }
+
+    // Manejar el clic en el botón de cerrar
+    document.querySelector(".close-button").addEventListener("click", function () {
+        // Ocultar el fondo difuminado y el formulario de edición
+        document.querySelector(".overlay").style.display = "none";
+        document.querySelector(".modal").style.display = "none";
+    });
+
+// Manejar el envío del formulario de edición
+const editarProductoForm = document.getElementById("editarProductoForm");
+editarProductoForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const id = document.getElementById("editarId").value;
+    const nombre = document.getElementById("editarNombre").value;
+    const imagen = document.getElementById("editarImagen").value;
+    const precio = parseFloat(document.getElementById("editarPrecio").value);
+
+    // Validar que el precio no sea negativo
+    if (precio < 0) {
+        alert("El precio no puede ser negativo.");
+        return;
+    }
+
+    // Encuentra el producto en el catálogo
+    const producto = catalogo.find((p) => p.id === id);
+
+    if (producto) {
+        // Actualizar los detalles del producto
+        producto.descripcion = nombre;
+        producto.imagen = imagen;
+        producto.precio = precio;
+
+        // Actualizar la tabla de productos
+        actualizarTablaProductos();
+
+        // Ocultar el formulario de edición
+        document.getElementById("formularioEdicion").style.display = "none";
+                // Ocultar el fondo difuminado y el formulario de edición
+                document.querySelector(".overlay").style.display = "none";
+                document.querySelector(".modal").style.display = "none";
+
+        // Guardar el catálogo actualizado en el almacenamiento local
+        localStorage.setItem("catalogo", JSON.stringify(catalogo));
+    }
+});
+
+
 
     cargarCatalogo();
 });
